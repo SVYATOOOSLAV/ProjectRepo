870ba9447ee74e7b54e9037cb68b7e03390f81a8
svyatoslav milkin <svyatoslavmilkin@yandex.ru>
2023-10-22 17:20:08 +0300

    First commit

---
+++ src/Test.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args) {
+        String str = "01:59:45";
+        String[] arrStr = str.split(":");
+        int sum = 0;
+
+        sum += Integer.parseInt(arrStr[0]) * 3600;
+        sum += Integer.parseInt(arrStr[1]) * 60;
+        sum += Integer.parseInt(arrStr[2]);
+
+        System.out.println(sum);
+
+    }
+}
